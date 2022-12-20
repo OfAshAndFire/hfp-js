31c3ebd50d4096a9a348fc6e2091cdedbad1d351
Daniel <daniel.ashcraft@ofashandfire.com>
2022-12-20 11:33:14 -0600

    Should now protect against zero's

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,32 @@
-export function HfpBinStringToFloat32 (str: string): number | Error {
+export function HfpBinStringToFloat (str: string): number | Error {
   if(str.length !== 32) return Error('Requires a 32 bit binary number')
-  let bin: number = parseInt(str, 2)
-
-  const sign: number = bin >>> 31 ? -1 : 1;  
-  let exp: number = (bin >>> 24 & 0x7f) - 64;
+  let bin: number = parseInt(str, 2) | 0;
   
-  // mask off the first 8 bits (those aren't used)
-  // convert the last 24 bits
-  let mantissa:string = (bin & 0xffffff).toString(2)
-
-  // converting from number to string in js loses leading 0s
-  // also, this is a decimal, so we have a 24 bit number, but we need 25 bit decimal
-  // we pad left until we reach the desired 25 bits
-  while (mantissa.length < 25) {
-    mantissa = `0`+ mantissa
+  let exp: number = -1;
+  let mantissa: number = bin & 0xffffff;
+  // protect against zeros
+  if (mantissa == 0) return bin < 0 ? -0 : 0;
+  while (mantissa < 0x800000) {
+    mantissa <<= 1; exp--;
   }
+  exp += 4 * (((bin >> 24) & 0x7f) - 64);
+  const sign: number = bin & 0x80000000
+  const f1: number = sign | ((exp + 1023) << 20) | ((mantissa & ~0x800000) >> 3);
+  const f0: number = (mantissa & ~0x800000) << 29;
+  return new Float64Array(new Int32Array([f0, f1]).buffer)[0];
+}
 
-  let float32: number = 0
-  for (let i:number = 0; Math.abs(i) < mantissa.length - 1; i -= 1) {
-    float32 += parseInt(mantissa[Math.abs(i)], 2) * Math.pow(2, i)
+export function HfpBinToFloat (bin: number): number | Error {
+  let exp: number = -1;
+  let mantissa: number = bin & 0xffffff;
+  // protect against zeros
+  if (mantissa == 0) return bin < 0 ? -0 : 0;
+  while (mantissa < 0x800000) {
+    mantissa <<= 1; exp--;
   }
-
-  return float32 * sign * Math.pow(16, exp);
+  exp += 4 * (((bin >> 24) & 0x7f) - 64);
+  const sign: number = bin & 0x80000000
+  const f1: number = sign | ((exp + 1023) << 20) | ((mantissa & ~0x800000) >> 3);
+  const f0: number = (mantissa & ~0x800000) << 29;
+  return new Float64Array(new Int32Array([f0, f1]).buffer)[0];
 }
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,45 +1,77 @@
-import { HfpBinStringToFloat32 } from '../src';
+import { HfpBinStringToFloat, HfpBinToFloat } from '../src';
 
 const positiveBinaryStringFromIbmDocs = "01000010011101101010000000000000"
-const positiveRealWorldString =         "01000010001100010111011011110001"
+
+// binary literals with leading 0 need prefixed by 0b
+const positiveBinaryFromIbmDocs = 0b01000010011101101010000000000000
+const positiveRealWorldString = "01000010001100010111011011110001"
+                
+// binary literals with leading 0 need prefixed by 0b
+const positiveRealWorldBinary = Buffer.from([0b01000010, 0b00110001, 0b01110110, 11110001])
 
 const negativeBinaryStringFromIbmDocs = "11000010011101101010000000000000"
+const negativeBinaryFromIbmDocs = 11000010011101101010000000000000
 const negativeRealWorldString = "11000010001100010111011011110001"
+const negativeRealWorldBinary = 11000010001100010111011011110001
+
+const testValue = "11000010100001100101111000100100"
+
 describe('Test IBM Hexadecimal Floating Point utils', () => {
   it('should convert positive binary as string, into floating point number', () => {
     // 118.625
-    let positiveValue = HfpBinStringToFloat32(positiveBinaryStringFromIbmDocs);
+    let positiveValue = HfpBinStringToFloat(positiveBinaryStringFromIbmDocs);
     expect(positiveValue).toBe(118.625)
 
     // real world example
-    let realWorldPositive = HfpBinStringToFloat32(positiveRealWorldString);
-    expect(realWorldPositive).toBe(49.464599609375)
+    let realWorldPositive = HfpBinStringToFloat(positiveRealWorldString);
+    expect(realWorldPositive).toBe(49.46461486816406)
   })
 
   it('Should convert positive numbers', () => {
     // -118.625 from ibm docs/wikipedia
-    let negativeValue = HfpBinStringToFloat32(negativeBinaryStringFromIbmDocs);
+    let negativeValue = HfpBinStringToFloat(negativeBinaryStringFromIbmDocs);
     expect(negativeValue).toBe(-118.625)
-    
-    let realWorldNegative = HfpBinStringToFloat32(negativeRealWorldString);
-    expect(realWorldNegative).toBe(-49.464599609375)
+
+    let realWorldNegative = HfpBinStringToFloat(negativeRealWorldString);
+    expect(realWorldNegative).toBe(-49.46461486816406)
   })
 
   it('should convert binary as string, into floating point number', () => {
     // -118.625 from ibm docs/wikipedia
-    let negativeValue = HfpBinStringToFloat32(negativeBinaryStringFromIbmDocs);
+    let negativeValue = HfpBinStringToFloat(negativeBinaryStringFromIbmDocs);
     expect(negativeValue).toBe(-118.625)
 
     // 118.625
-    let positiveValue = HfpBinStringToFloat32(positiveBinaryStringFromIbmDocs);
+    let positiveValue = HfpBinStringToFloat(positiveBinaryStringFromIbmDocs);
     expect(positiveValue).toBe(118.625)
 
     // real world example
-    let realWorldPositive = HfpBinStringToFloat32(positiveRealWorldString);
-    expect(realWorldPositive).toBe(49.464599609375)
+    let realWorldPositive = HfpBinStringToFloat(positiveRealWorldString);
+    expect(realWorldPositive).toBe(49.46461486816406)
 
-    let realWorldNegative = HfpBinStringToFloat32(negativeRealWorldString);
-    expect(realWorldNegative).toBe(-49.464599609375)
+    let realWorldNegative = HfpBinStringToFloat(negativeRealWorldString);
+    expect(realWorldNegative).toBe(-49.46461486816406)
   })
 
+  it('should read test value', () => {
+    const buff: Buffer = Buffer.from([
+      66,
+      49,
+      118,
+      241
+  ])
+
+    console.time('HfpBinStringToFloat')
+    const test300 = HfpBinToFloat(buff.readInt32BE())
+    console.timeEnd('HfpBinStringToFloat')
+    expect(test300).toBe(49.46461486816406)
+  })
+
+  it('Should not error on zero', () => {
+    const zero = HfpBinToFloat(0)
+    expect(zero).toBe(0)
+
+    const nZero = HfpBinToFloat(-0)
+    expect(nZero).toBe(0)
+  })
 })
